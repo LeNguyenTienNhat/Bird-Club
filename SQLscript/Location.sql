898a516c31422586e02d97651621bd5e9e309d99
Shido2907 <117149113+Shido2907@users.noreply.github.com>
2023-06-23 22:02:18 +0700

    Update Location.sql

--- SQLscript/Location.sql
+++ SQLscript/Location.sql
@@ -1,20 +1,20 @@
 INSERT INTO [Location]
-VALUES ('LID0', 'Q1', 'Q1')
+VALUES ('LID0', 'Q1', '07 Công Trường Lam Sơn, Bến Nghé, Quận 1, Thành phố Hồ Chí Minh 700000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID1', 'Q3', 'Q3')
+VALUES ('LID1', 'Q3', '64A Trương Định, Võ Thị Sáu, Quận 3, Thành phố Hồ Chí Minh, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID2', 'Q4', 'Q4')
+VALUES ('LID2', 'Q4', '300A Nguyễn Tất Thành, Phường 13, Quận 4, Thành phố Hồ Chí Minh 70000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID3', 'Q10', 'Q10')
+VALUES ('LID3', 'Q10', '11 Đ. Sư Vạn Hạnh, Phường 12, Quận 10, Thành phố Hồ Chí Minh, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID4', 'Q12', 'Q12')
+VALUES ('LID4', 'Q12', '170 Lê Văn Khương, Thới An, Quận 12, Thành phố Hồ Chí Minh, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID5', 'Q9', 'Q9')
+VALUES ('LID5', 'Q9', 'Lô E2a-7, Đường D1, Đ. D1, Long Thạnh Mỹ, Thành Phố Thủ Đức, Thành phố Hồ Chí Minh 700000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID6', 'Q2', 'Q2')
+VALUES ('LID6', 'Q2', '125 Đồng Văn Cống, Phường Thạnh Mỹ Lợi, Quận 2, Thành phố Hồ Chí Minh 700000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID7', 'Q.Dong Da', 'Q.Dong Da')
+VALUES ('LID7', 'Q.Dong Da', '86 P. Nguyễn Văn Tuyết, Trung Liệt, Đống Đa, Hà Nội 700000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID8', 'Q.Ba Dinh', 'Q.Ba Dinh')
+VALUES ('LID8', 'Q.Ba Dinh', 'Đ. Bưởi, Thủ Lệ, Ba Đình, Hà Nội 100000, Vietnam')
 INSERT INTO [Location]
-VALUES ('LID9', 'Q.Tay Ho', 'Q.Tay Ho')
+VALUES ('LID9', 'Q.Tay Ho', '275 Đ. Âu Cơ, Quảng An, Tây Hồ, Hà Nội, Vietnam')
