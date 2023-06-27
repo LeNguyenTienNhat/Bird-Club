8fa8a7af68056cc6204c78d80d3f94f5766e9f5a
Le Nguyen Tien Nhat <xlenhat1602x@gmail.com>
2023-06-27 20:39:36 +0700

    Add view birds for staff role

--- Bird-club/src/main/java/com/fptuni/prj301/demo/controller/StaffHomepage.java
+++ Bird-club/src/main/java/com/fptuni/prj301/demo/controller/StaffHomepage.java
@@ -1,36 +1,31 @@
-
 package com.fptuni.prj301.demo.controller;
+
+import com.fptuni.prj301.demo.model.UserSession;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class StaffHomepage extends HttpServlet {
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            String action = request.getParameter("action");
-            
-            if (action == null || action.equals("staffhome")) {
+        String action = request.getParameter("action");
 
-                RequestDispatcher rd = request.getRequestDispatcher("staff_homepage.jsp");
-                rd.forward(request, response);                
-            }            
-            
-            else if (action.equals("events")) {
-                request.setAttribute("action", "viewevents");
-                RequestDispatcher rd = request.getRequestDispatcher("StaffEvents");
-                rd.forward(request, response);
-            }
-
+        if (action == null || action.equals("staffhome")) {
+            RequestDispatcher rd = request.getRequestDispatcher("staff_homepage.jsp");
+            rd.forward(request, response);
+        } 
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
