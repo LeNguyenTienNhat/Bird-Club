15e518a4ab8a492aeb7ac9c1542b8c0dd671210c
Shido2907 <117149113+Shido2907@users.noreply.github.com>
2023-06-29 00:40:04 +0700

    Update membership.sql

--- SQLscript/membership.sql
+++ SQLscript/membership.sql
@@ -1,11 +1,11 @@
 INSERT INTO [Membership]
-VALUES('MID01','Individual',100000,'1','The individual membership package for bird enthusiasts. You will have access to all activities of the bird club for 1 month.');
+VALUES('MID01','Individual',100000,'1','The individual membership package for bird enthusiasts. You will have access to all activities of the bird club for 1 month.','available');
 
 INSERT INTO [Membership]
-VALUES('MID02','Family',200000,'3','The family membership package for bird-loving families. You and your family will have access to all activities of the bird club for 3 months.');
+VALUES('MID02','Family',200000,'3','The family membership package for bird-loving families. You and your family will have access to all activities of the bird club for 3 months.','available');
 
 INSERT INTO [Membership]
-VALUES('MID03','Family Extend',400000,'6','The extended family membership package for bird-loving families. You and your family will have access to all activities of the bird club for 6 months.');
+VALUES('MID03','Family Extend',400000,'6','The extended family membership package for bird-loving families. You and your family will have access to all activities of the bird club for 6 months.','available');
 
 INSERT INTO [Membership]
-VALUES('MID00','Staff',100000,'12','This package is for staff.');
+VALUES('MID00','Staff',100000,'12','This package is for staff.','available');
