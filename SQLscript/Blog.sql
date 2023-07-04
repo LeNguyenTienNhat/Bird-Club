eda9b4888d281b25f41643c733ceac03c0196709
Shido2907 <117149113+Shido2907@users.noreply.github.com>
2023-07-04 09:55:28 +0700

    Update Blog.sql

--- SQLscript/Blog.sql
+++ SQLscript/Blog.sql
@@ -1,23 +1,23 @@
 Insert into [Blog]
-Values ('BLID1','Their melodious songs dance through the air, filling our hearts with pure delight.','Red-whiskered bulbul','12-02-2023','UID7','12','https://i.pinimg.com/736x/ec/ee/9e/ecee9ef90028b5865583fc2528b5175c.jpg','idle');
+Values ('BLID1','Their melodious songs dance through the air, filling our hearts with pure delight.','Red-whiskered bulbul','12-02-2023','UID7','0','https://i.pinimg.com/736x/ec/ee/9e/ecee9ef90028b5865583fc2528b5175c.jpg','idle');
 Insert into [Blog]
-Values ('BLID2','Watching their acrobatic flights and playful stunts is a source of endless entertainment.','Red-whiskered bulbul','03-15-2023','UID13','34','https://res-3.cloudinary.com/ebirdr/image/upload/s--eJlN_eG6--/f_auto,q_auto,t_full/2654-red-whiskered-bulbul.jpg','idle');
+Values ('BLID2','Watching their acrobatic flights and playful stunts is a source of endless entertainment.','Red-whiskered bulbul','03-15-2023','UID13','0','https://res-3.cloudinary.com/ebirdr/image/upload/s--eJlN_eG6--/f_auto,q_auto,t_full/2654-red-whiskered-bulbul.jpg','idle');
 Insert into [Blog]
-Values ('BLID3','The crimson whiskers that adorn their cheeks are like tiny bursts of joy.','Red-whiskered bulbul','11-30-2022','UID24','74','https://content.eol.org/data/media/28/c6/0f/18.https___www_inaturalist_org_photos_1145051.jpg','idle');
+Values ('BLID3','The crimson whiskers that adorn their cheeks are like tiny bursts of joy.','Red-whiskered bulbul','11-30-2022','UID24','0','https://content.eol.org/data/media/28/c6/0f/18.https___www_inaturalist_org_photos_1145051.jpg','idle');
 Insert into [Blog]
-Values ('BLID4','Red-whiskered bulbuls thrive in the company of their flock, spreading warmth and camaraderie.','Red-whiskered bulbul','12-08-2022','UID33','63','https://delicioustacos.files.wordpress.com/2020/12/800px-red-whiskered_bulbul_.jpg?w=768','idle');
+Values ('BLID4','Red-whiskered bulbuls thrive in the company of their flock, spreading warmth and camaraderie.','Red-whiskered bulbul','12-08-2022','UID33','0','https://delicioustacos.files.wordpress.com/2020/12/800px-red-whiskered_bulbul_.jpg?w=768','idle');
 Insert into [Blog]
-Values ('BLID5','These bulbuls are diligent garden helpers, keeping pests at bay and maintaining nature is balance.','Red-whiskered bulbul','06-25-2023','UID12','12','https://3.bp.blogspot.com/-WI7DX9fbeos/T6028snRbcI/AAAAAAAASZw/2rPHA36EKiA/s1600/redwhiskered_bulbul_copy1.jpg','denied');
+Values ('BLID5','These bulbuls are diligent garden helpers, keeping pests at bay and maintaining nature is balance.','Red-whiskered bulbul','06-25-2023','UID12','0','https://3.bp.blogspot.com/-WI7DX9fbeos/T6028snRbcI/AAAAAAAASZw/2rPHA36EKiA/s1600/redwhiskered_bulbul_copy1.jpg','denied');
 Insert into [Blog]
-Values ('BLID6','Their vibrant plumage is a testament to the artist brushstrokes of nature palette.','Red-whiskered bulbul','11-02-2023','UID41','4','https://c2.staticflickr.com/6/5510/14177796138_1de2ce5a80_b.jpg','denied');
+Values ('BLID6','Their vibrant plumage is a testament to the artist brushstrokes of nature palette.','Red-whiskered bulbul','11-02-2023','UID41','0','https://c2.staticflickr.com/6/5510/14177796138_1de2ce5a80_b.jpg','denied');
 Insert into [Blog]
-Values ('BLID7','The chirps and calls of red-whiskered bulbuls greet each day with a symphony of welcoming notes.','Red-whiskered bulbul','04-22-2023','UID7','42','https://wallpapercave.com/wp/wp4424844.jpg','denied');
+Values ('BLID7','The chirps and calls of red-whiskered bulbuls greet each day with a symphony of welcoming notes.','Red-whiskered bulbul','04-22-2023','UID7','0','https://wallpapercave.com/wp/wp4424844.jpg','denied');
 Insert into [Blog]
-Values ('BLID8','Witnessing their nurturing instincts and devoted care for their young is a heartwarming sight.','Red-whiskered bulbul','02-22-2023','UID54','41','https://www.pixellicious.com/wp-content/uploads/2016/12/201-1024x768.jpg','denied');
+Values ('BLID8','Witnessing their nurturing instincts and devoted care for their young is a heartwarming sight.','Red-whiskered bulbul','02-22-2023','UID54','0','https://www.pixellicious.com/wp-content/uploads/2016/12/201-1024x768.jpg','denied');
 Insert into [Blog]
-Values ('BLID9','Red-whiskered bulbuls possess a magnetic charm that captivates and enchants all who encounter them.','Red-whiskered bulbul','01-24-2023','UID23','22','https://c2.staticflickr.com/8/7788/18039432128_419c3decb4_b.jpg','denied');
+Values ('BLID9','Red-whiskered bulbuls possess a magnetic charm that captivates and enchants all who encounter them.','Red-whiskered bulbul','01-24-2023','UID23','0','https://c2.staticflickr.com/8/7788/18039432128_419c3decb4_b.jpg','denied');
 Insert into [Blog]
-Values ('BLID10','Their graceful flight patterns, combined with their striking colors, create a visual spectacle.','Red-whiskered bulbul','06-15-2023','UID22','32','https://www.birdsofmurrieta.com/uploads/7/5/8/4/75844173/red-whiskered-bulbul-pycnonotus-jocosus-apr07-010.jpg','denied');
+Values ('BLID10','Their graceful flight patterns, combined with their striking colors, create a visual spectacle.','Red-whiskered bulbul','06-15-2023','UID22','0','https://www.birdsofmurrieta.com/uploads/7/5/8/4/75844173/red-whiskered-bulbul-pycnonotus-jocosus-apr07-010.jpg','denied');
 Insert into [Blog]
 Values ('BLID11','The harmonious blend of their songs, vibrant plumage, and playful nature is a sensory delight.','Red-whiskered bulbul','06-29-2023','UID11','42','https://i.ytimg.com/vi/qUj2Hi0IlJg/maxresdefault.jpg','approved');
 Insert into [Blog]
