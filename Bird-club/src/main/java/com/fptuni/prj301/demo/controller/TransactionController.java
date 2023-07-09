8bd75047743f16068656a4d5920a722ef2811992
ducvx <122201412+ducvx@users.noreply.github.com>
2023-07-09 19:33:16 +0700

    location

--- Bird-club/src/main/java/com/fptuni/prj301/demo/controller/TransactionController.java
+++ Bird-club/src/main/java/com/fptuni/prj301/demo/controller/TransactionController.java
@@ -5,17 +5,21 @@
  */
 package com.fptuni.prj301.demo.controller;
 
+import com.fptuni.prj301.demo.dbmanager.LocationManager;
 import com.fptuni.prj301.demo.dbmanager.TransactionManager;
 import com.fptuni.prj301.demo.dbmanager.UserAccessManager;
+import com.fptuni.prj301.demo.model.Location;
 import com.fptuni.prj301.demo.model.Transaction;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import tool.utils.UIDGenerator;
 
 @WebServlet(name = "TransactionController", urlPatterns = {"/TransactionController"})
@@ -33,11 +37,11 @@ public class TransactionController extends HttpServlet {
             String TT = request.getParameter("TT");
             String doc = request.getParameter("docT");
             String name = request.getParameter("name");
-            if(name!=null){
-                UserAccessManager u =new UserAccessManager();
-                uid =u.findUIDByName(name);
+            if (name != null) {
+                UserAccessManager u = new UserAccessManager();
+                uid = u.findUIDByName(name);
             }
-            
+
             Transaction transaction = new Transaction();
             transaction.setPID(PID);
             transaction.setUID(uid);
@@ -48,16 +52,26 @@ public class TransactionController extends HttpServlet {
 
             TransactionManager transactionManager = new TransactionManager();
             boolean insertionSuccess = transactionManager.insertTransaction(transaction);
-            if(uid ==null){request.getRequestDispatcher("/.jsp").forward(request, response);}
+            if (uid == null) {
+                request.getRequestDispatcher("/.jsp").forward(request, response);
+            }
             if (insertionSuccess) {
                 request.setAttribute("successMessage", "Transaction saved successfully");
                 request.getRequestDispatcher("/payment_success.jsp").forward(request, response);
                 return;
-            }else {
+            } else {
                 request.setAttribute("successMessage", "Transaction saved successfully");
                 request.getRequestDispatcher("/payment_fail.jsp").forward(request, response);
                 return;
             }
+        }
+        if (action != null && action.equals("location")) {
+            LocationManager l = new LocationManager();
+            List<Location> locations = l.getList();
+             HttpSession ss = request.getSession(true);
+            ss.setAttribute("location", locations);
+            
+           response.sendRedirect(request.getContextPath() +"/member_locations.jsp");
         }
 
         // Handle insertion failure if needed
